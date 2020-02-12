6ed252c3327c0b74fa9257f804e9287feb24d114
Manuel Leduc <manuel.leduc@inria.fr>
2020-02-12 09:56:23 +0100

    separated front and back ends

--- federun-backend/src/main/java/fr/mleduc/service/user/TokenProviderImpl.java
+++ federun-backend/src/main/java/fr/mleduc/service/user/TokenProviderImpl.java
@@ -6,7 +6,6 @@ import com.auth0.jwt.JWTVerifier;
 import com.auth0.jwt.algorithms.Algorithm;
 import com.auth0.jwt.interfaces.Claim;
 import com.auth0.jwt.interfaces.DecodedJWT;
-import fr.mleduc.expt.NotImplementedException;
 import fr.mleduc.security.profile.Role;
 import org.eclipse.microprofile.config.inject.ConfigProperty;
 
--- federun-backend/src/main/java/fr/mleduc/service/user/UserService.java
+++ federun-backend/src/main/java/fr/mleduc/service/user/UserService.java
@@ -1,10 +1,10 @@
 package fr.mleduc.service.user;
 
+import fr.mleduc.panache.FederunUser;
 import fr.mleduc.expt.EmailAlreadyExistsException;
 import fr.mleduc.expt.InvalidPasswordException;
 import fr.mleduc.expt.UserNotFoundException;
 import fr.mleduc.expt.UsernameAlreadyExistsException;
-import fr.mleduc.panache.FederunUser;
 
 public interface UserService {
     FederunUser login(String email, String password) throws UserNotFoundException, InvalidPasswordException;
