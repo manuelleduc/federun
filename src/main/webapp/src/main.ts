6a9c7e7bc682db760d243c233f098ff2ad629fa9
Manuel Leduc <manuel.leduc@xwiki.com>
2022-05-14 14:13:38 +0000

    Vue router integration

--- src/main/webapp/src/main.ts
+++ src/main/webapp/src/main.ts
@@ -1,9 +1,25 @@
 import { createApp } from "vue";
 import { createPinia } from "pinia";
+import { RouteRecordRaw, createRouter, createWebHashHistory } from "vue-router";
 import App from "./App.vue";
+import Home from "./components/Home.vue";
+import About from "./components/About.vue";
+
+const routes: Array<RouteRecordRaw> = [
+  { path: "/", name: "Home", component: Home },
+  { path: "/about", name: "About", component: About },
+];
+
+const router = createRouter({
+  history: createWebHashHistory(),
+  routes,
+});
 
 const app = createApp(App);
 
+app.config.performance = true;
+
 app.use(createPinia());
+app.use(router);
 
 app.mount("#app");
