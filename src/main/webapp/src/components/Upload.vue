1e433f5fc8d55ae9bcd5afb306715791ca759b14
Manuel Leduc <manuel.leduc@xwiki.com>
2022-05-15 22:13:35 +0200

    Minimal file upload
    - integration of axios
    - binding of the input file field

--- src/main/webapp/src/components/Upload.vue
+++ src/main/webapp/src/components/Upload.vue
@@ -1,10 +1,29 @@
 <script setup lang="ts">
+import type {Ref, UnwrapRef} from 'vue'
+import {ref} from 'vue'
+import axios from "axios";
+
+const myfile: Ref<UnwrapRef<any>> = ref(undefined)
+
+function upload() {
+  // TODO: move to pinia.
+  const formData = new FormData()
+  formData.append('myfile', myfile.value.files[0])
+  axios.post('/gpx/upload', formData, {
+    headers: {
+      'Content-Type': 'multipart/form-data'
+    }
+  })
+
+  // Clear the file field.
+  myfile.value.value = null;
+}
 </script>
 
 <template>
-  <form action="/gpx/upload" method="post" enctype="multipart/form-data">
-    <input type="file" name="myfile" />
-    <input type="submit" />
+  <form>
+    <input type="file" ref="myfile"/>
+    <input type="submit" @click.prevent="upload($event)"/>
   </form>
 </template>
 
--- src/main/webapp/src/main.ts
+++ src/main/webapp/src/main.ts
@@ -1,20 +1,21 @@
-import { createApp } from "vue";
-import { createPinia } from "pinia";
-import { RouteRecordRaw, createRouter, createWebHashHistory } from "vue-router";
+import {createApp} from "vue";
+import {createPinia} from "pinia";
+import type {RouteRecordRaw} from 'vue-router'
+import {createRouter, createWebHashHistory} from "vue-router";
 import App from "./App.vue";
 import Home from "./components/Home.vue";
 import About from "./components/About.vue";
 import Upload from './components/Upload.vue';
 
-const routes: Array<RouteRecordRaw> = [
-  { path: "/", name: "Home", component: Home },
-  { path: "/upload", name: "Upload", component: Upload }, // TODO: define an upload component
-  { path: "/about", name: "About", component: About },
+const routes: RouteRecordRaw[] = [
+    {path: "/", name: "Home", component: Home},
+    {path: "/upload", name: "Upload", component: Upload},
+    {path: "/about", name: "About", component: About},
 ];
 
 const router = createRouter({
-  history: createWebHashHistory(),
-  routes,
+    history: createWebHashHistory(),
+    routes,
 });
 
 const app = createApp(App);
